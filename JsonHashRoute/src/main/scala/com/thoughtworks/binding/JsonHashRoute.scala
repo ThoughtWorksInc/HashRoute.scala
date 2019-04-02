a919b0162c12f998829c59fa89f638144755be26
Yang Bo <atryyang@thoughtworks.com>
2019-04-02 10:18:43 +0800

    Fix updateState

--- JsonHashRoute/src/main/scala/com/thoughtworks/binding/JsonHashRoute.scala
+++ JsonHashRoute/src/main/scala/com/thoughtworks/binding/JsonHashRoute.scala
@@ -3,14 +3,5 @@ import com.thoughtworks.binding.Binding.Var
 import org.scalajs.dom.{Window, window}
 import upickle.default._
 
-import scala.scalajs.js.URIUtils.decodeURIComponent
-
 class JsonHashRoute[PageState: Reader: Writer](state: Var[PageState], window: Window = window)
-    extends HashRoute[PageState](
-      state, {
-        case hashText if hashText.startsWith("#") =>
-          read[PageState](decodeURIComponent(hashText.substring(1)))
-      },
-      write(_),
-      window
-    )
+    extends HashRoute[PageState](state, read[PageState](_), write(_), window)
